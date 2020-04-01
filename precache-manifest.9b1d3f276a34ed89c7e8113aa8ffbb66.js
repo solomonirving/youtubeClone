45de2ecf3ab7030666cbd1b7fb8140790f6c75cb
Solomon Irving <solomon.irving@gmail.com>
2020-03-31 18:23:44 -0700

    Updates

---
+++ precache-manifest.9b1d3f276a34ed89c7e8113aa8ffbb66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e89958e1c1f184fdab94992455b62b",
+    "url": "/youtubeClone/index.html"
+  },
+  {
+    "revision": "9646312e32032c51be6c",
+    "url": "/youtubeClone/static/js/2.6911db76.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtubeClone/static/js/2.6911db76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a3a5e396fb56097989f",
+    "url": "/youtubeClone/static/js/main.1acfdf4e.chunk.js"
+  },
+  {
+    "revision": "287c6e8f232f1ba92350",
+    "url": "/youtubeClone/static/js/runtime-main.8b90a46e.js"
+  }
+]);
